3c9ddbed7ba6e50e5d87d7d71b3232a5b20f2ac5
Dave McPherson <dave@hnry.io>
2023-08-03 15:25:49 +1000

    adds an aliases file for use in ~/.oh-my-zsh/custom

---
+++ aliases.zsh
@@ -0,0 +1,8 @@
+alias reload!='. ~/.zshrc'
+
+alias cls='clear' # Good 'ol Clear Screen command
+
+alias be="bundle exec"
+alias hg="history | grep"
+
+alias gitlg="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'"
